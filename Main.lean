079371cd43594ef745cfb2a468dbad05e6d6062c
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:15:13 +0900

    Verify final subsp theorem axioms

--- Main.lean
+++ Main.lean
@@ -1,19 +1,9 @@
 import Subsp
 import Subsp.new.subsp
 
-def testDecLt {lam : Nat} (a b : new.T lam) : Decidable (a < b) :=
-  inferInstance
-
-def testDecEq {lam : Nat} (a b : new.T lam) : Decidable (a = b) :=
-  inferInstance
-
-#print axioms testDecLt
-#print axioms testDecEq
-#print axioms new.T_total
-#print axioms new.T.exists_G_not_lt
-#print axioms new.T.decForallMem
-#print axioms new.T.NFComp_of_ZeroDom
-#print axioms new.T.SDom_G_closed
+#print axioms new.T.fund_omega_NFComp_closed
+#print axioms new.T.fund_iter_NFComp
+#print axioms new.T.fund_NF_closed
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
