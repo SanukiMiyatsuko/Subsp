d40366b3acb0a6f17e4ce239e15b15341360edf4
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 06:23:51 +0900

    Remove closed conversion proof steps

--- Subsp/new/subsp.lean
+++ Subsp/new/subsp.lean
@@ -508,13 +508,11 @@ theorem T.fund_PZ_some {lam : Nat}
         lhs
         rw [T.fund, if_pos rfl]
         rw [hmin]
-      rfl
     | succ m' =>
       conv =>
         lhs
         rw [T.fund, if_pos rfl]
         rw [hmin]
-      rfl
 
 theorem T.mul_PZ_lt_of_compareVec_lt {lam : Nat}
     (u v : Vec (T lam) lam) (t : T lam)
