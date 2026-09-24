734b80b858310d0c8d30bf2689b66324330df5a3
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:11:38 +0900

    Narrow constructive axiom dependency source

--- Main.lean
+++ Main.lean
@@ -1,18 +1,17 @@
 import Subsp
 import Subsp.new.subsp
 
-#print axioms new.T.isNF_G_isNFComp
-#print axioms new.T.NFComp_of_ZeroDom
+#print axioms new.T.exists_G_not_lt
 #print axioms new.T.G_size_lt
 #print axioms new.T.G_trans
-#print axioms new.T.exists_G_not_lt
+#print axioms new.T.decForallMem
 #print axioms new.T.find_violating_source
-#print axioms new.T.GZ_lt_of_NFComp_lt
-#print axioms new.T.SDom_G_lt_upper
 #print axioms new.T.SDom_G_closed
-#print axioms new.T.NFComp_of_SDom
 #print axioms new.T.NFComp_of_SDom_Z_or_eq
-#print axioms new.T.fund_one_NFComp_closed
+#print axioms new.T.fund_one_master
+#print axioms new.T.fund_omega_master
+#print axioms new.T.fund_Omega_master
+#print axioms new.T.fund_iter_NFComp_core
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
