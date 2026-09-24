c14b968a54bb285b5b0394bcbb5bc9279284bc56
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:15:51 +0900

    Verify final theorem axiom dependencies

--- Main.lean
+++ Main.lean
@@ -1,9 +1,6 @@
 import Subsp
 import Subsp.new.subsp
 
-#print axioms new.T.decIsNF
-#print axioms new.Vec.decAllNF
-#print axioms new.Vec.decAllG
 #print axioms new.T.fund_omega_NFComp_closed
 #print axioms new.T.fund_iter_NFComp
 #print axioms new.T.fund_NF_closed
