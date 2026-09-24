8058fe0577515b9456072946e9706af54e873fe1
taketori <naoki123453@gmail.com>
2026-09-25 07:32:03 +0900

    GPT製証明を追加

--- Subsp/new/stop.lean
+++ Subsp/new/stop.lean
@@ -5,15 +5,23 @@ import Subsp.new.Base
 import Subsp.new.subsp
 import Subsp.new.trans
 
-theorem new.T.OT_iff_NF (lam : Nat) (s : T lam) : isOT lam s ↔ isNF s ∧ (1 < lam → s < P (Vec.ofFn lam (fun x => if x.val = 1 then P (Vec.ofFn lam (fun y => Z)) Z else Z)) Z) := sorry
+theorem new.T.OT_iff_NF (lam : Nat) (s : T lam) :
+  isOT lam s ↔ isNF s ∧
+    (1 < lam → s < P (Vec.ofFn lam (fun x => if x.val = 1 then P (Vec.ofFn lam (fun y => Z)) Z else Z)) Z) := by
+  sorry
 
-theorem NF_is_NF1 (lam : Nat) (s : new.T lam) : new.T.isNF s → T.isNF1 (trans s) := sorry
+theorem NF_is_NF1 (lam : Nat) (s : new.T lam) :
+  new.T.isNF s → T.isNF1 (trans s) := by
+  sorry
 
-theorem order_embeding (lam : Nat) (s t : new.T lam) (hs : new.T.isNF s) (ht : new.T.isNF t) : s < t ↔ trans s < trans t := sorry
+theorem order_embeding (lam : Nat) (s t : new.T lam) (hs : new.T.isNF s) (ht : new.T.isNF t) :
+  s < t ↔ trans s < trans t := by
+  sorry
 
 def new.T.NF (lam : Nat) := { s : T lam // isNF s }
 
-theorem wellfounded_NF (lam : Nat) : WellFounded (fun s t : new.T.NF lam => s.val < t.val) := sorry
+theorem wellfounded_NF (lam : Nat) : WellFounded (fun s t : new.T.NF lam => s.val < t.val) := by
+  sorry
 
 def T.isSubNF (n : Nat) (s : T) :=
   isNF1 s ∧
@@ -22,8 +30,10 @@ def T.isSubNF (n : Nat) (s : T) :=
     | 1 => s < P 0 (P 1 Z Z) Z
     | n' + 1 => s < P 0 (P 1 (P 1 (mul (P 1 Z Z) (ofNat n')) Z) Z) Z
 
-theorem OT_iff_NF1 (lam : Nat) (s : new.T lam) : new.T.isOT lam s ↔ T.isSubNF lam (trans s) := sorry
+theorem OT_iff_NF1 (lam : Nat) (s : new.T lam) : new.T.isOT lam s ↔ T.isSubNF lam (trans s) := by
+  sorry
 
 def new.T.OT (lam : Nat) := { s : T lam // isOT lam s }
 
-theorem wellfounded_OT (lam : Nat) : WellFounded (fun s t : new.T.OT lam => s.val < t.val) := sorry
+theorem wellfounded_OT (lam : Nat) : WellFounded (fun s t : new.T.OT lam => s.val < t.val) := by
+  sorry
