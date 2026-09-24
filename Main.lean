52d5ea95e1ad033c9d75b495cc0f079769932642
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:09:29 +0900

    Trace axiom dependencies in subsp proofs

--- Main.lean
+++ Main.lean
@@ -1,23 +1,14 @@
 import Subsp
 import Subsp.new.subsp
 
-#print axioms new.T.fund_lt_self
-#print axioms new.T.head_fund_le
-#print axioms new.T.isNF_G_isNFComp
-#print axioms new.T.rplc_min_NFComp_closed
-#print axioms new.T.fund_one_master
-#print axioms new.T.fund_one_NFComp_closed
-#print axioms new.T.NFComp_of_SDom
-#print axioms new.T.NFComp_of_SDom_Z_or_eq
-#print axioms new.T.SDom_PZ_pivot_comp
-#print axioms new.T.rplc_NF_closed
-#print axioms new.T.rplc_two_NF_closed
-#print axioms new.T.mul_PZ_NF_closed
-#print axioms new.T.SDom_tail
+#print axioms new.T.fund_omega_master
+#print axioms new.T.fund_dom_master
 #print axioms new.T.fund_Omega_master
 #print axioms new.T.fund_iter_NFComp_core
-#print axioms new.T.fund_dom_master
-#print axioms new.T.fund_one_arg_irrel
+#print axioms new.T.NFComp_of_SDom_Z_or_eq
+#print axioms new.T.SDom_G_closed
+#print axioms new.T.find_violating_source
+#print axioms new.T.fund_one_master
 #print axioms new.T.fund_omega_NFComp_closed
 #print axioms new.T.fund_iter_NFComp
 #print axioms new.T.fund_NF_closed
