f55afa9c45092b3ed22172bdea946517ac7df11f
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:07:08 +0900

    Check axioms of completed subsp proofs

--- Main.lean
+++ Main.lean
@@ -1,4 +1,9 @@
 import Subsp
+import Subsp.new.subsp
+
+#print axioms new.T.fund_omega_NFComp_closed
+#print axioms new.T.fund_iter_NFComp
+#print axioms new.T.fund_NF_closed
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
