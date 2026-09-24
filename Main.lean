b7e8fea31b5269b1499b341fb5a946c86e7a8bea
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:16:03 +0900

    Restore Main after final proof verification

--- Main.lean
+++ Main.lean
@@ -1,9 +1,4 @@
 import Subsp
-import Subsp.new.subsp
-
-#print axioms new.T.fund_omega_NFComp_closed
-#print axioms new.T.fund_iter_NFComp
-#print axioms new.T.fund_NF_closed
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
