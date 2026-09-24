61dc3cfa2096ffbdc6301f8088c076e44f2d2296
SanukiMiyatsuko <132545189+SanukiMiyatsuko@users.noreply.github.com>
2026-09-25 07:10:10 +0900

    Narrow constructive axiom trace

--- Main.lean
+++ Main.lean
@@ -1,18 +1,24 @@
 import Subsp
 import Subsp.new.subsp
 
-#print axioms new.T.fund_one_master
-#print axioms new.T.fund_one_NFComp_closed
+#print axioms new.T.Z_le
+#print axioms new.T_total
+#print axioms new.T.le_refl
+#print axioms new.T.le_trans
+#print axioms new.T.lt_of_le_of_lt
+#print axioms new.T.lt_of_lt_of_le
+#print axioms new.T.isNF_G_isNFComp
+#print axioms new.T.NFComp_of_ZeroDom
+#print axioms new.T.G_size_lt
+#print axioms new.T.G_trans
+#print axioms new.T.exists_G_not_lt
+#print axioms new.T.find_violating_source
+#print axioms new.T.GZ_lt_of_NFComp_lt
+#print axioms new.T.SDom_G_lt_upper
+#print axioms new.T.SDom_G_closed
 #print axioms new.T.NFComp_of_SDom
-#print axioms new.T.SDom_PZ_pivot_comp
-#print axioms new.T.fund_Omega_master
-#print axioms new.T.fund_iter_NFComp_core
-#print axioms new.T.fund_dom_master
-#print axioms new.T.fund_omega_master
-#print axioms new.T.fund_one_arg_irrel
-#print axioms new.T.fund_omega_NFComp_closed
-#print axioms new.T.fund_iter_NFComp
-#print axioms new.T.fund_NF_closed
+#print axioms new.T.NFComp_of_SDom_Z_or_eq
+#print axioms new.T.fund_one_NFComp_closed
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
